477d78d74e8b8b8e19449dca74e4190d9e0562be
Tim Lee <tlee87@gmail.com>
2020-12-20 21:45:11 -0500

    improve ui

--- pages/album.js
+++ pages/album.js
@@ -21,10 +21,10 @@ const videos = [
   "https://tennis-swings.s3.amazonaws.com/timuserid/2020_12_18_1152_59/tim_ground_profile_wide_540p_clip_1_swing_7.mp4",
   "https://tennis-swings.s3.amazonaws.com/timuserid/2020_12_18_1152_59/tim_ground_profile_wide_540p_clip_2_swing_1.mp4",
   "https://tennis-swings.s3.amazonaws.com/timuserid/2020_12_18_1152_59/tim_ground_profile_wide_540p_clip_2_swing_2.mp4",
-  "https://tennis-swings.s3.amazonaws.com/timuserid/2020_12_18_1152_59/tim_ground_profile_wide_540p_clip_2_swing_3.mp4",
-  "https://tennis-swings.s3.amazonaws.com/timuserid/2020_12_18_1152_59/tim_ground_profile_wide_540p_clip_2_swing_4.mp4",
-  "https://tennis-swings.s3.amazonaws.com/timuserid/2020_12_18_1152_59/tim_ground_profile_wide_540p_clip_2_swing_5.mp4",
-  "https://tennis-swings.s3.amazonaws.com/timuserid/2020_12_18_1152_59/tim_ground_profile_wide_540p_clip_2_swing_6.mp4",
+  // "https://tennis-swings.s3.amazonaws.com/timuserid/2020_12_18_1152_59/tim_ground_profile_wide_540p_clip_2_swing_3.mp4",
+  // "https://tennis-swings.s3.amazonaws.com/timuserid/2020_12_18_1152_59/tim_ground_profile_wide_540p_clip_2_swing_4.mp4",
+  // "https://tennis-swings.s3.amazonaws.com/timuserid/2020_12_18_1152_59/tim_ground_profile_wide_540p_clip_2_swing_5.mp4",
+  // "https://tennis-swings.s3.amazonaws.com/timuserid/2020_12_18_1152_59/tim_ground_profile_wide_540p_clip_2_swing_6.mp4",
 ]
 
 
@@ -123,7 +123,7 @@ const Album = ({ startClock, tick }) => {
       <div className="p-8 flex flex-wrap">
         { videos.map( (videoUrl, i) => {
           return (
-            <div className="flex flex-col m-4 w-1/6 h-1/4 content-center justify-center items-center">
+            <div className="flex flex-col p-2 m-4 w-1/6 h-1/4 content-center justify-center items-center rounded shadow-md">
               <ReactPlayer
                 className="rounded-md overflow-hidden"
                 ref={playerRefs[i]}
@@ -134,16 +134,23 @@ const Album = ({ startClock, tick }) => {
                 muted={true}
                 playbackRate={playbackRate}
                 loop={true}
-                progressInterval={250}
+                progressInterval={200}
+                onProgress={({ played }) => setPlayerDurations({
+                  ...playerDurations,
+                  [i]: parseFloat((Math.ceil(played/.05)*.05).toFixed(2)),
+                })}
                 height=""
                 width=""
               />
 
-              <div className="flex flex-row content-center justify-center p-1 my-1">
+              {/* Controls Panel */}
+              <div className="flex flex-row content-center justify-center p-1 mt-4 bg-gray-100 rounded">
+                {/* Picture in Picture */}
                 { pips[i] &&
                   <input type='button'
-                    className='border rounded p-0.5 mx-1 text-xs'
+                    className='border rounded p-0.5 mx-1 text-xs bg-blue-400'
                     value='-'
+                    tabIndex={(i*3)+1}
                     onClick={() => {
                       const newPips = pips.map((p,j) => j === i ? false : p)
                       setPips(newPips)
@@ -152,8 +159,9 @@ const Album = ({ startClock, tick }) => {
                 }
                 { !pips[i] &&
                   <input type='button'
-                    className='border rounded p-0.5 mx-1 text-xs'
+                    className='border rounded p-0.5 mx-1 text-xs bg-blue-400'
                     value='+'
+                    tabIndex={(i*3)+1}
                     onClick={() => {
                       const newPips = pips.map((p,j) => j === i ? true : p)
                       setPips(newPips)
@@ -161,10 +169,12 @@ const Album = ({ startClock, tick }) => {
                   />
                 }
 
+                {/* Play / Pause */}
                 { playings[i] &&
                   <input type='button'
-                    className='border bg-gray-200 rounded p-0.5 mx-1 text-xs'
+                    className='border w-10 bg-gray-200 rounded p-0.5 mx-1 text-xs bg-red-700 text-white'
                     value='pause'
+                    tabIndex={(i*3)+2}
                     onClick={() => {
                       const newPlayings = playings.map((p,j) => j === i ? false : p)
                       setPlayings(newPlayings)
@@ -173,8 +183,9 @@ const Album = ({ startClock, tick }) => {
                 }
                 { !playings[i] &&
                   <input type='button'
-                    className='border bg-gray-200 rounded p-0.5 mx-1 text-xs'
+                    className='border w-10 bg-gray-200 rounded p-0.5 mx-1 text-xs bg-green-700 text-white'
                     value='play'
+                    tabIndex={(i*3)+2}
                     onClick={() => {
                       const newPlayings = playings.map((p,j) => j === i ? true : p)
                       setPlayings(newPlayings)
@@ -185,19 +196,26 @@ const Album = ({ startClock, tick }) => {
                     }}
                   />
                 }
-
-                { playerDurations[i] &&
-                  <div className="bg-white rounded p-0.5 mx-1 text-xs">
-                    <span> { playerDurations[i] }/10</span>
-                  </div>
-                }
                 
-                <input type='range'
+                {/* Seek */}
+                <input
+                  type='range'
+                  tabIndex={(i*3)+3}
+                  value={playerDurations[i]}
                   min={0}
-                  max={0.999999}
-                  step='0.1'
+                  max={1}
+                  step='0.05'
                   onChange={handleSeekChange(playerRefs[i], i)}
+                  onFocus={ e => {
+                    console.log('focus!')
+                    e.stopPropagation()
+                    e.preventDefault()
+                  }}
                 />
+
+                <div className="bg-white rounded p-0.5 mx-1 text-xs">
+                  <span> { playerDurations[i] ? playerDurations[i].toFixed(2) : '0.00' }/1.0</span>
+                </div>
               </div>
             </div>
           )

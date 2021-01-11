e181c9b2ef644d04a384735f1b448943fcd7afd4
Tim Lee <tlee87@gmail.com>
2021-01-10 22:47:34 -0500

    add navigate albums

--- pages/albums/index.js
+++ pages/albums/index.js
@@ -22,18 +22,21 @@ const AlbumsIndex = ({
   const [friendsAlbumsPage, setFriendsAlbumsPage] = useState(0)
   const [publicAlbumsPage, setPublicAlbumsPage] = useState(0)
 
-  const myActiveAlbums = albums.myAlbums.slice(myAlbumsPage * albumsPerRow, (myAlbumsPage+1) * albumsPerRow)
-  const friendsActiveAlbums = albums.friendsAlbums.slice(friendsAlbumsPage * albumsPerRow, (friendsAlbumsPage+1) * albumsPerRow)
-  const publicActiveAlbums = albums.publicAlbums.slice(publicAlbumsPage * albumsPerRow, (publicAlbumsPage+1) * albumsPerRow)
+  const myActiveAlbums = albums.myAlbums.slice(myAlbumsPage * albumsPerRow, (myAlbumsPage+1) * albumsPerRow).filter( a => !!a )
+  console.log("myalbums", myActiveAlbums)
+  const friendsActiveAlbums = albums.friendsAlbums.slice(friendsAlbumsPage * albumsPerRow, (friendsAlbumsPage+1) * albumsPerRow).filter( a => !!a )
+  const publicActiveAlbums = albums.publicAlbums.slice(publicAlbumsPage * albumsPerRow, (publicAlbumsPage+1) * albumsPerRow).filter( a => !!a )
 
   useEffect(() => {
+    console.log("loading ablums...")
     loadAlbums()
   }, [])
 
   useEffect(() => {
+    console.log("setting players...")
     const activeAlbums = [...myActiveAlbums, ...friendsActiveAlbums, ...publicActiveAlbums]
     setPlayerRefs(ref => activeAlbums.map((_, i) => ref[i] || createRef()))
-    setPlayings(activeAlbums.map(() => true))
+    setPlayings(activeAlbums.map(() => false))
     setPips(activeAlbums.map(() => false))
   }, [albums.myAlbums, myAlbumsPage, albums.friendsAlbums, friendsAlbumsPage, albums.publicAlbums, publicAlbumsPage])
 
@@ -49,6 +52,9 @@ const AlbumsIndex = ({
   }
 
   const renderVideo = ({ swing, i, ref, playing, pip, duration }) => {
+    if (!swing) {
+      return null
+    }
     return(
       <Fragment>
         <ReactPlayer
@@ -179,8 +185,28 @@ const AlbumsIndex = ({
           {/* Start My Albums */}
 
           <div className="flex flex-col">
-            <h2>My Albums</h2>
-            <div className="p-4 flex flex-wrap w-4/5">
+            <div className="flex flex-row">
+              <h2>My Albums</h2>
+              <div className="p-4 flex flex-wrap w-4/5">
+                { myAlbumsPage > 0 &&
+                <button
+                  onClick={() => setMyAlbumsPage(myAlbumsPage-1)}
+                  className="border border-black rounder p-0.5 mx-1"
+                >
+                  &lt;
+                </button>
+                }
+                { (myAlbumsPage < (albums.myAlbums.length / albumsPerRow)-1) &&
+                <button
+                  onClick={() => setMyAlbumsPage(myAlbumsPage+1)}
+                  className="border border-black rounder p-0.5 mx-1"
+                >
+                  &gt;
+                </button>
+                }
+              </div>
+            </div>
+            <div className="flex flex-row">
               { myActiveAlbums.map( (album, i) => {
                 return (
                   <div key={i}
@@ -206,8 +232,28 @@ const AlbumsIndex = ({
           {/* Start Friends Albums */}
 
           <div className="flex flex-col">
-            <h2>Friends Albums</h2>
-            <div className="p-4 flex w-4/5">
+            <div className="flex flex-row">
+              <h2>Friends Albums</h2>
+              <div className="p-4 flex w-4/5">
+                { friendsAlbumsPage > 0 &&
+                <button
+                  onClick={() => setFriendsAlbumsPage(friendsAlbumsPage-1)}
+                  className="border border-black rounder p-0.5 mx-1"
+                >
+                  &lt;
+                </button>
+                }
+                { (friendsAlbumsPage < (albums.friendsAlbums.length / albumsPerRow)-1) &&
+                <button
+                  onClick={() => setMyAlbumsPage(friendsAlbumsPage+1)}
+                  className="border border-black rounder p-0.5 mx-1"
+                >
+                  &gt;
+                </button>
+                }
+              </div>
+            </div>
+            <div className="flex flex-row">
               { friendsActiveAlbums.map( (album, i) => {
                 const idx = i + myActiveAlbums.length
                 return (
@@ -234,8 +280,30 @@ const AlbumsIndex = ({
           {/* Start Public Albums */}
 
           <div className="flex flex-col">
-            <h2>Public Albums</h2>
-            <div className="p-4 flex w-4/5">
+            <div className="flex flex-row">
+              <div className="flex flex-col">
+                <h2>Public Albums</h2>
+                <div className="p-4 flex w-4/5">
+                  { publicAlbumsPage > 0 &&
+                    <button
+                      onClick={() => setPublicAlbumsPage(publicAlbumsPage-1)}
+                      className="border border-black rounder p-0.5 mx-1"
+                    >
+                      &lt;
+                    </button>
+                  }
+                  { (publicAlbumsPage < (albums.publicAlbums.length / albumsPerRow)-1) &&
+                    <button
+                      onClick={() => setMyAlbumsPage(publicAlbumsPage+1)}
+                      className="border border-black rounder p-0.5 mx-1"
+                    >
+                      &gt;
+                    </button>
+                  }
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-row">
               { publicActiveAlbums.map( (album, i) => {
                 const idx = i + (myActiveAlbums.length) + (friendsActiveAlbums.length)
                 return (
@@ -258,6 +326,7 @@ const AlbumsIndex = ({
               })}
             </div>
           </div>
+          
         </div>
         {/* End Album Videos */}
       </main>

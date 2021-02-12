adcffa66a2cd6e2f457385a5d7c928f30ebae947
Tim Lee <tlee87@gmail.com>
2021-02-11 20:05:12 -0500

    fix tooltip

--- pages/albums/[...id].js
+++ pages/albums/[...id].js
@@ -398,7 +398,7 @@ const Album = ({
             <div className="flex flex-col text-sm">
               {/* Album Overview */}
               <div className="mb-2">
-                <div className="flex flex-row content-center justify-center items-center mb-2 relative static">
+                <div className="flex flex-row content-center justify-center items-center mb-2 relative">
                   <h2 className="text-gray-300 uppercase cursor-pointer text-center"
                     onClick={() => {
                       if (activeSideBar === "Album Overview") {
@@ -410,26 +410,28 @@ const Album = ({
                   >
                   Album Overview
                   </h2>
-                  <input type="button"
-                    className="text-xs rounded-full bg-black text-white hover:bg-white hover:text-black h-4 w-4 border border-white ml-2 cursor-pointer hidden lg:block"
-                    value="?"
-                    onClick={() => {
-                      setShowOverviewUsage(activeSideBar !== "Album Overview" ? true : !showOverviewUsage)
-                      setActiveSidebar("Album Overview")
-                    }}
-                  />
-                  { showOverviewUsage &&
-                    <div className="absolute w-72 -my-40 -mr-40 bg-yellow-300 text-black text-xs font-semibold tracking-wide rounded shadow py-1.5 px-4 bottom-full">
-                      <ul className="list-disc ml-2">
+                  <div className="relative">
+                    <input type="button"
+                      className="text-xs rounded-full bg-black text-white hover:bg-white hover:text-black h-4 w-4 border border-white ml-2 cursor-pointer hidden lg:block"
+                      value="?"
+                      onClick={() => {
+                        setShowOverviewUsage(activeSideBar !== "Album Overview" ? true : !showOverviewUsage)
+                        setActiveSidebar("Album Overview")
+                      }}
+                    />
+                    { showOverviewUsage &&
+                    <div className="absolute ml-10 -my-28 w-60 bg-yellow-300 text-black text-xs font-semibold tracking-wide rounded shadow py-1.5 px-4 bottom-full z-10">
+                      <ul className="list-disc pl-6">
                         <li>Shows the swings and rallies that were clipped from the original video</li>
                         <li>Rallies, or Points, are consecutive hits while playing tennis</li>
                         <li>Toggle the Rally checkboxes to filter by Rally</li>
                       </ul>
                     </div>
-                  }
+                    }
+                  </div>
                 </div>
                 { activeSideBar === "Album Overview" &&
-                  <div className="flex flex-col rounded bg-white shadow-lg p-2 mb-2 h-96 overflow-scroll">
+                  <div className="flex flex-col rounded bg-white shadow-lg p-2 mb-2 overflow-scroll">
                     <div className="flex flex-col sticky left-0 content-center justify-center items-start pl-8 py-4 mb-4 rounded shadow-lg bg-gray-200 text-gray-700">
                       <p className="uppercase underline font-semibold mb-1">
                         { album?.swingVideos?.length } Total Swings | { swingsByRally.length } Rallies
@@ -443,6 +445,7 @@ const Album = ({
                               [] :
                               swingsByRally.map( swings => swings[0].rally )
                             setFilteredRallies(rallies)
+                            setAlbumPage(0)
                           }}
                         />
                         <span className="font-semibold mr-1">
@@ -462,6 +465,7 @@ const Album = ({
                                       filteredRallies.filter( rally => rally != i+1) :
                                       [...filteredRallies, i+1]
                                     setFilteredRallies(rallies)
+                                    setAlbumPage(0)
                                   }}
                                 />
                                 <span className="font-semibold mr-1">Rally {i+1}:</span>
@@ -471,6 +475,7 @@ const Album = ({
                           })
                         }
                       </div>
+                      
                       <a href="#"
                         className="text-blue-700 underline cursor-pointer my-2"
                         onClick={() => setShowGraph(!showGraph)}

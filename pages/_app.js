d6b2fe298a2281858d2713934580dd13cccf14f6
Tim Lee <tlee87@gmail.com>
2021-03-05 12:02:29 -0500

    refactor

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import React from "react"
-import { wrapper } from "../state/store"
+import { store } from "../state/store"
 import "../styles/index.css"
 import {PersistGate} from "redux-persist/integration/react"
-import {useStore, useSelector} from "react-redux"
+import {useStore, Provider} from "react-redux"
 import Head from "next/head"
 import PropTypes from "prop-types"
 
@@ -15,17 +15,14 @@ import colors from "../styles/colors.js"
 
 const PAGE_URL = "tennis-community-web.vercel.app"
 
-export default wrapper.withRedux(({ Component, pageProps, ...otherProps }) => {
-  const store = useStore()
-  // const { head: { desc, title, img }} = useSelector(state => state)
-  console.log("wrapper", pageProps)
+const App = ({ Component, pageProps }) => {
   const desc = pageProps.head?.desc
   const title = pageProps.head?.title
   const img = pageProps.head?.img
 
-  return (
-    <PersistGate persistor={store.__persistor}>
-      {/* <Head>
+  return(
+    <Provider store={store}>
+      <Head>
         <title>Hive Tennis</title>
         <meta name="description" content={desc} key="desc"/>
 
@@ -42,15 +39,59 @@ export default wrapper.withRedux(({ Component, pageProps, ...otherProps }) => {
         <meta name="twitter:title" content={title} key="twitter_title"/>
         <meta name="twitter:description" content={desc} key="twitter_desc"/>
         <meta name="twitter:image" content={img} key="twitter_img"/>
-      </Head> */}
+      </Head>
       <ThemeProvider theme={ colors }>
         { <FlashNotification /> }
         <NavBar />
         <Component {...pageProps} />
       </ThemeProvider>
-    </PersistGate>
+    </Provider>
   )
-})
+}
+
+App.propTypes = {
+  Component: PropTypes.func,
+  pageProps: PropTypes.object,
+}
+
+export default App
+
+// export default wrapper.withRedux(({ Component, pageProps, ...otherProps }) => {
+//   const store = useStore()
+//   // const { head: { desc, title, img }} = useSelector(state => state)
+//   console.log("wrapper", pageProps)
+//   const desc = pageProps.head?.desc
+//   const title = pageProps.head?.title
+//   const img = pageProps.head?.img
+
+//   return (
+//     <PersistGate persistor={store.__persistor}>
+//       {/* <Head>
+//         <title>Hive Tennis</title>
+//         <meta name="description" content={desc} key="desc"/>
+
+//         <meta property="og:url" content={`https://${PAGE_URL}/`} key="ogurl" name="ogurl"/>
+
+//         <meta property="og:type" content="website" key="ogtype" name="ogtype"/>
+//         <meta property="og:title" content={title} key="ogtitle" name="ogtitle"/>
+//         <meta property="og:description" content={desc} key="ogdesc" name="ogdesc"/>
+//         <meta property="og:image" content={img} key="ogimg" name="ogimg"/>
+
+//         <meta name="twitter:card" content="summary_large_image" key="twitter_card"/>
+//         <meta property="twitter:domain" content={PAGE_URL} key="twitter_dom" name="twitter_dom"/>
+//         <meta property="twitter:url" content={`https://${PAGE_URL}/`} key="twitter_url" name="twitter_url"/>
+//         <meta name="twitter:title" content={title} key="twitter_title"/>
+//         <meta name="twitter:description" content={desc} key="twitter_desc"/>
+//         <meta name="twitter:image" content={img} key="twitter_img"/>
+//       </Head> */}
+//       <ThemeProvider theme={ colors }>
+//         { <FlashNotification /> }
+//         <NavBar />
+//         <Component {...pageProps} />
+//       </ThemeProvider>
+//     </PersistGate>
+//   )
+// })
 
 // function WrappedApp({ Component, pageProps, ...otherProps }) {
 //   const store = useStore()
--- state/store.js
+++ state/store.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware, combineReducers } from "redux"
-import { HYDRATE, createWrapper } from "next-redux-wrapper"
 import thunkMiddleware from "redux-thunk"
-import logger from "redux-logger"
+import { createLogger } from "redux-logger"
 
 import {
   flashNotificationReducer,
@@ -27,16 +26,7 @@ import {
   albumsInitialState,
 } from "./album/reducer"
 
-
-const bindMiddleware = (middleware) => {
-  if (process.env.NODE_ENV !== "production") {
-    const { composeWithDevTools } = require("redux-devtools-extension")
-    return composeWithDevTools(applyMiddleware(...middleware))
-  }
-  return applyMiddleware(...middleware)
-}
-
-const combinedReducer = combineReducers({
+export const rootReducer = combineReducers({
   head: headReducer,
   confirmation: confirmationReducer,
   flashNotification: flashNotificationReducer,
@@ -48,65 +38,18 @@ const combinedReducer = combineReducers({
   navBar: navBarReducer,
 })
 
+const loggerMiddleware = createLogger()
+
+export const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  )
+)
+
 export const LOG_OUT = "LOG_OUT"
 
 export const logOut = () => ({
   type: LOG_OUT
 })
-
-const reducer = (state, action) => {
-  if (action.type === HYDRATE) {
-    return {
-      ...state, // use previous state
-      ...action.payload, // apply delta from hydration
-    }
-  } else if (action.type === LOG_OUT) {
-    return {
-      // UI
-      head: headInitialState,
-      flashNotification: flashNotificationInitialState,
-      navBar: navBarInitialState,
-      // user
-      confirmation: confirmationInitialState,
-      user: userInitialState,
-      usersCache: usersCacheInitialState,
-      // uploads
-      recentUploads: recentUploadsInitialState,
-      // albums
-      album: albumInitialState,
-      albums: albumsInitialState,
-    }
-  }
-  else {
-    return combinedReducer(state, action)
-  }
-}
-
-const makeConfiguredStore = (reducer) =>
-  createStore(reducer, undefined, applyMiddleware(logger))
-
-const initStore = () => {
-  const isServer = typeof window === "undefined"
-  if (isServer) {
-    return makeConfiguredStore(reducer)
-  } else {
-    // we need it only on client side
-    const {persistStore, persistReducer} = require("redux-persist")
-    const storage = require("redux-persist/lib/storage").default
-
-    const persistConfig = {
-      key: "root",
-      whitelist: ["user"], // make sure it does not clash with server keys
-      storage
-    }
-
-    const persistedReducer = persistReducer(persistConfig, reducer)
-    const store = createStore(persistedReducer, bindMiddleware([thunkMiddleware]))
-
-    store.__persistor = persistStore(store) // Nasty hack
-
-    return store
-  }
-}
-
-export const wrapper = createWrapper(initStore)

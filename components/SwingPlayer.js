1aba013f36a7f5d561dd7c4bb2749c8c7dd39fdf
Tim Lee <tlee87@gmail.com>
2021-03-10 08:55:50 -0500

    try light

--- components/SwingPlayer.js
+++ components/SwingPlayer.js
@@ -37,83 +37,81 @@ const SwingPlayerContent = ({
   const ref = playerRefs[i]
   const pip = pips[i]
   const playing = playings[i]
-  console.log("inViewport", swing.name, inViewport)
   return (
     <div className="flex flex-col justify-center items-center m-1 p-1 bg-gray-400 rounded shadow-lg"
       ref={forwardedRef}
     >
-      { inViewport ?
-        <>
-          <ReactPlayer
-            ref={ref}
-            url={swing.videoURL} 
-            playing={playing}
-            pip={pip}
-            volume={0}
-            muted={true}
-            playbackRate={playbackRate}
-            loop={true}
-            progressInterval={200}
-            onProgress={({ played }) => {
-              const frame = Math.round(played*swingFrames)
-              setPlayerFrames({
-                ...playerFrames,
-                [i]: frame,
-              })
-            }}
-            width={playerWidth || ""}
-            height={playerHeight || ""}
-            config={{
-              file: {
-                attributes: {
-                  controlsList: "nofullscreen",
-                  playsInline: true,
-                }
-              }
-            }}
-          />
+      <ReactPlayer
+        ref={ref}
+        url={swing.videoURL}
+        light={inViewport ? false : swing.jpgURL}
+        playing={playing}
+        pip={pip}
+        volume={0}
+        muted={true}
+        playbackRate={playbackRate}
+        loop={true}
+        progressInterval={200}
+        onProgress={({ played }) => {
+          const frame = Math.round(played*swingFrames)
+          setPlayerFrames({
+            ...playerFrames,
+            [i]: frame,
+          })
+        }}
+        width={playerWidth || ""}
+        height={playerHeight || ""}
+        config={{
+          file: {
+            attributes: {
+              controlsList: "nofullscreen",
+              playsInline: true,
+            }
+          }
+        }}
+      />
 
-          {/* Controls Panel */}
-          <div className="flex flex-col content-center justify-center p-1 mt-1 w-full bg-gray-100 rounded">
-            <div className="flex flex-row content-center justify-center items-center">
-              <a className="text-xs text-blue-400 underline mr-1 cursor-pointer"
-                onClick={() => router.push({
-                  pathname: `/albums/${albumId}`,
-                  query: { swing: swing.id },
-                })}
-              >
+      {/* Controls Panel */}
+      <div className="flex flex-col content-center justify-center p-1 mt-1 w-full bg-gray-100 rounded">
+        <div className="flex flex-row content-center justify-center items-center">
+          <a className="text-xs text-blue-400 underline mr-1 cursor-pointer"
+            onClick={() => router.push({
+              pathname: `/albums/${albumId}`,
+              query: { swing: swing.id },
+            })}
+          >
               Swing { swing.name } 
-              </a>
-              <div className="relative">
-                <div className="text-xs">| Rally { swing.rally }</div>
-                { (showAlbumUsage && i === 0) &&
+          </a>
+          <div className="relative">
+            <div className="text-xs">| Rally { swing.rally }</div>
+            { (showAlbumUsage && i === 0) &&
                 <div className="absolute w-40 mb-14 -mx-14 bg-yellow-300 text-black text-xs font-semibold tracking-wide rounded shadow py-1.5 px-4 bottom-full z-100">
                   Rally # in original video
                   <svg className="absolute text-yellow-300 h-2 right-0 mr-20 top-full" x="0px" y="0px" viewBox="0 0 600 400" xmlSpace="preserve"><polygon className="fill-current" points="0,0 300,400 600,0"/></svg>
                 </div>
-                }
-              </div>
+            }
+          </div>
 
-              <div className="flex flex-row bg-white rounded p-0.5 mx-1 text-xs relative">
+          <div className="flex flex-row bg-white rounded p-0.5 mx-1 text-xs relative">
               @<div className="w-8 text-center">{ `${parseInt(swing.timestampSecs/60)}:${parseInt(swing.timestampSecs%60).toString().padStart(2,"0")}` }</div>
-                { (showAlbumUsage && i === 0) &&
+            { (showAlbumUsage && i === 0) &&
                 <div className="absolute w-32 mb-2 mx-3 bg-yellow-300 text-black text-xs font-semibold tracking-wide rounded shadow py-1.5 px-4 bottom-full z-100">
                   Timestamp in original video
                   <svg className="absolute text-yellow-300 h-2 left-0 ml-3 top-full" x="0px" y="0px" viewBox="0 0 600 400" xmlSpace="preserve"><polygon className="fill-current" points="0,0 300,400 600,0"/></svg>
                 </div>
-                }
-              </div>
+            }
+          </div>
 
-              <div className={`flex flex-row bg-white ${(swing.comments?.length || 0) !== 0 && "border-2 border-blue-400"} rounded p-0.5 mx-1 text-xs`}>
-                {(swing.comments?.length || 0)}
-                <IconContext.Provider value={{ color: (swing.comments?.length || 0) === 0 ? "black" : "blue" }}>
-                  <div className="mx-1 mt-0.5 cursor-pointer">
-                    <ImBubbles2 />
-                  </div>
-                </IconContext.Provider>
+          <div className={`flex flex-row bg-white ${(swing.comments?.length || 0) !== 0 && "border-2 border-blue-400"} rounded p-0.5 mx-1 text-xs`}>
+            {(swing.comments?.length || 0)}
+            <IconContext.Provider value={{ color: (swing.comments?.length || 0) === 0 ? "black" : "blue" }}>
+              <div className="mx-1 mt-0.5 cursor-pointer">
+                <ImBubbles2 />
               </div>
+            </IconContext.Provider>
+          </div>
 
-              { isOwner &&
+          { isOwner &&
               <IconContext.Provider value={{ color: "red" }}>
                 <div className="mx-2 content-center justify-center items-center cursor-pointer"
                   onClick={onDelete}
@@ -121,19 +119,19 @@ const SwingPlayerContent = ({
                   <BsTrash/>
                 </div>
               </IconContext.Provider>
-              }
-            </div>
+          }
+        </div>
           
-            <div className="flex flex-row content-center justify-center">
-              {/* Picture in Picture */}
-              <div className="relative">
-                { (showAlbumUsage && i === 0) &&
+        <div className="flex flex-row content-center justify-center">
+          {/* Picture in Picture */}
+          <div className="relative">
+            { (showAlbumUsage && i === 0) &&
               <div className="absolute w-48 mb-2 -mx-40 bg-yellow-300 text-black text-xs font-semibold tracking-wide rounded shadow py-1.5 px-4 bottom-full z-100">
                 View Picture-In-Picture for a draggable video
                 <svg className="absolute text-yellow-300 h-2 right-0 mr-3 top-full" x="0px" y="0px" viewBox="0 0 600 400" xmlSpace="preserve"><polygon className="fill-current" points="0,0 300,400 600,0"/></svg>
               </div>
-                }
-                { pip &&
+            }
+            { pip &&
               <IconContext.Provider value={{ color: "blue", height: "8px", width: "8px" }}>
                 <div className="mx-2 items-stretch content-center justify-center items-center cursor-pointer">
                   <RiPictureInPictureExitFill onClick={() => {
@@ -142,8 +140,8 @@ const SwingPlayerContent = ({
                   }}/>
                 </div>
               </IconContext.Provider>
-                }
-                { !pip &&
+            }
+            { !pip &&
               <IconContext.Provider value={{ color: "blue", height: "8px", width: "8px" }}>
                 <div className="mx-2 items-stretch content-center justify-center items-center cursor-pointer">
                   <RiPictureInPicture2Fill onClick={() => {
@@ -152,11 +150,11 @@ const SwingPlayerContent = ({
                   }}/>
                 </div>
               </IconContext.Provider>
-                }
-              </div>
+            }
+          </div>
             
-              {/* Play / Pause */}
-              { playing &&
+          {/* Play / Pause */}
+          { playing &&
               <IconContext.Provider value={{ color: "red" }}>
                 <div className="mx-2 content-center justify-center items-center cursor-pointer">
                   <FaRegPauseCircle onClick={() => {
@@ -165,8 +163,8 @@ const SwingPlayerContent = ({
                   }}/>
                 </div>
               </IconContext.Provider>
-              }
-              { !playing &&
+          }
+          { !playing &&
               <IconContext.Provider value={{ color: "blue" }}>
                 <div className="mx-2 content-center justify-center items-center cursor-pointer">
                   <FaPlayCircle onClick={() => {
@@ -179,41 +177,36 @@ const SwingPlayerContent = ({
                   }}/>
                 </div>
               </IconContext.Provider>
-              }
+          }
             
-              {/* Seek */}
-              <input
-                type='range'
-                tabIndex={(i*3)+3}
-                value={duration}
-                min={0}
-                max={swingFrames}
-                step='1'
-                onChange={handleSeekChange(ref, i)}
-                onFocus={ e => {
-                  e.stopPropagation()
-                  e.preventDefault()
-                }}
-              />
+          {/* Seek */}
+          <input
+            type='range'
+            tabIndex={(i*3)+3}
+            value={duration}
+            min={0}
+            max={swingFrames}
+            step='1'
+            onChange={handleSeekChange(ref, i)}
+            onFocus={ e => {
+              e.stopPropagation()
+              e.preventDefault()
+            }}
+          />
 
-              <div className="flex flex-row content-center justify-center items-center">
-                <div className="bg-white rounded p-0.5 mx-1 text-xs relative">
-                  { (showAlbumUsage && i ===0) &&
+          <div className="flex flex-row content-center justify-center items-center">
+            <div className="bg-white rounded p-0.5 mx-1 text-xs relative">
+              { (showAlbumUsage && i ===0) &&
                   <div className="absolute -mb-16 w-48 bg-yellow-300 text-black text-xs font-semibold tracking-wide rounded shadow py-1.5 px-4 bottom-full z-100">
                     <svg className="absolute text-yellow-300 h-2 left-0 ml-3 bottom-full" x="0px" y="0px" viewBox="0 0 600 400" xmlSpace="preserve"><polygon className="fill-current" points="0,400 300,0 600,400"/></svg>
                     Frame # / Total Frames              
                   </div>
-                  }
-                  <div className="w-8 text-center">{ duration ? duration.toString().padStart(2, "0") : "00" }/{swingFrames}</div>
-                </div>
-              </div>
+              }
+              <div className="w-8 text-center">{ duration ? duration.toString().padStart(2, "0") : "00" }/{swingFrames}</div>
             </div>
           </div>
-        </>
-        :
-        <div />
-      }
-      
+        </div>
+      </div>
     </div>
   )
 }
